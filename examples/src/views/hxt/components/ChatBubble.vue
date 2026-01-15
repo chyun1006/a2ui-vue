faa29030f87998d757760d4304c1b0dca0a86386
chyun1006 <746804915@qq.com>
2026-01-15 20:03:11 +0800

    none

--- examples/src/views/hxt/components/ChatBubble.vue
+++ examples/src/views/hxt/components/ChatBubble.vue
@@ -81,7 +81,7 @@ const timeString = computed(() => {
         v-else-if="isUser"
         class="bg-blue-600 text-white rounded-2xl rounded-tr-sm px-4 py-2.5 shadow-md text-sm leading-relaxed"
       >
-        {{ message.content }}123
+        {{ message.content }}
       </div>
 
       <!-- Agent Widget/Content -->
